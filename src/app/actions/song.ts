04a98d43d4592b4f6ff2011767e211d1f22c58f4
Otto Uusihakala <otto.uusihakala@gmail.com>
2024-12-26 21:39:10 +0200

    Installed Spotify API types, react-qr-code, typed song search

--- src/app/actions/song.ts
+++ src/app/actions/song.ts
@@ -2,14 +2,14 @@
 
 import spotifySession from "../spotifySession";
 
-export const search = async () => {
+export const search = async (): Promise<SpotifyApi.TrackSearchResponse> => {
   const yearRange = '1970-1979';
   const req = await fetch(`https://api.spotify.com/v1/search?q=year:${yearRange}&type=track&market=FI&limit=1`, {
     headers: {
       'Authorization': await spotifySession.getBearerTokenHeader()
     }
   });
-  const jsonRes = await req.json();
+  const jsonRes = await req.json() as SpotifyApi.TrackSearchResponse;
   console.log('search jsonRes', jsonRes);
   return jsonRes;
 }
