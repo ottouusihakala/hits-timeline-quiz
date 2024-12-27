4d4b153e7c67a2c6b2a5f63aa13f589a3a61aa4f
Otto Uusihakala <otto.uusihakala@gmail.com>
2024-12-27 17:58:11 +0200

    Python script improvements - fetches tracks from multiple playlists - groups tracks by release decade
    Additionally WIP rendering to an image file.

--- generate-cards.py
+++ generate-cards.py
@@ -2,6 +2,8 @@ from dotenv import load_dotenv, dotenv_values
 import qrcode
 import requests
 import os
+import json
+from PIL import Image
 from base64 import b64encode
 
 FILTR_US_80S_HITS_TOP_100_PLAYLIST_ID = "19PgP2QSGPcm6Ve8VhbtpG"
@@ -13,6 +15,15 @@ FILTR_US_90S_HITS_TOP_100_PLAYLIST_ID = "3C64V048fGyQfCjmu9TIGA"
 FILTR_00S_HITS_PLAYLIST_ID = "1U3x51O0LQ4TtaD5CgxuGL"
 # Has 538 tracks (2024-12-27)
 BANGERS_2010S_PLAYLIST_ID = "357fWKFTiDhpt9C69CMG4q"
+
+playlist_ids = [
+    FILTR_SWE_60S_HITS_PLAYLIST_ID, 
+    FILTR_US_70S_HITS_TOP_100_PLAYLIST_ID, 
+    FILTR_US_80S_HITS_TOP_100_PLAYLIST_ID, 
+    FILTR_US_90S_HITS_TOP_100_PLAYLIST_ID,
+    FILTR_00S_HITS_PLAYLIST_ID,
+    BANGERS_2010S_PLAYLIST_ID
+]
 
 load_dotenv()
 
@@ -31,9 +42,53 @@ auth_token_res = requests.post(
 
 auth_token = auth_token_res.json()
 
-tracks_res = requests.get(
-    url="https://api.spotify.com/v1/playlists/19PgP2QSGPcm6Ve8VhbtpG/tracks",
-    headers={"Authorization": "Bearer " + auth_token.get("access_token")},
-)
+tracks_by_decades = {
+    "60s": [],
+    "70s": [],
+    "80s": [],
+    "90s": [],
+    "2000s": [],
+    "2010s": []
+}
 
-print("tracks_res", tracks_res.text)
+fields_filter = "items(id,track(name,album(name,release_date,release_date_precision),artists(name),external_urls(spotify))),total"
+
+for playlist_id in playlist_ids:
+    tracks_res = requests.get(
+        url="https://api.spotify.com/v1/playlists/" + playlist_id + "/tracks?fields=" + fields_filter,
+        headers={"Authorization": "Bearer " + auth_token.get("access_token")},
+    )
+    tracks_json = tracks_res.json()
+    tracks_amount = tracks_json.get("total")
+    tracks = tracks_json.get("items")
+    if tracks_amount > 100:
+        tracks = tracks[0:100]
+    for track in tracks:
+        release_year = int(track.get("track").get("album").get("release_date")[0:4])
+        if release_year >= 1960 and release_year < 1970:
+            tracks_by_decades["60s"].append(track)
+        elif release_year >= 1970 and release_year < 1980:
+            tracks_by_decades["70s"].append(track)
+        elif release_year >= 1980 and release_year < 1990:
+            tracks_by_decades["80s"].append(track)
+        elif release_year >= 1990 and release_year < 2000:
+            tracks_by_decades["90s"].append(track)
+        elif release_year >= 2000 and release_year < 2010:
+            tracks_by_decades["2000s"].append(track)
+        elif release_year >= 2010 and release_year < 2020:
+            tracks_by_decades["2010s"].append(track)
+
+with open("tracks.json", "w") as f:
+    json.dump(tracks_by_decades, f)
+
+# 2 inches, 300 pixels per inch, 2 * 300
+card_size_x = 2 * 300
+card_size_y = 3.5 * 300
+
+for decade in tracks_by_decades:
+    for track in decade:
+        card = Image.new(mode="RGBA", size=(card_size_x, card_size_y), color=0)
+        qr_code = qrcode.make(track.get("track").get("external_urls").get("spotify"))
+        card.paste(qr_code)
+        card.show()
+    
