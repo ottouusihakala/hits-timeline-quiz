8e5d6ee3fef08a0b3e7025eb1adcc57f2061a524
Otto Uusihakala <otto.uusihakala@gmail.com>
2024-12-31 14:43:33 +0200

    Render card: QR code on front side and track information on back side

--- create-card-test.py
+++ create-card-test.py
@@ -4,7 +4,7 @@ from PIL import Image, ImageDraw2, ImageDraw, ImageFont
 
 track = {
     "track": {
-        "artists": [{ "name": "The Tokens" }],
+        "artists": [{ "name": "The Tokens" }, { "name": "Dummy Artist" }],
         "album": {
             "name": "The Lion Sleeps Tonight",
             "release_date": "1994-08-11",
@@ -21,23 +21,24 @@ CARD_WIDTH = 600
 CARD_HEIGHT = 900
 COOPER_HEWITT_BOLD_FONT = "CooperHewitt-Bold.otf"
 
-font = ImageFont.truetype(COOPER_HEWITT_BOLD_FONT)
+font = ImageFont.truetype(COOPER_HEWITT_BOLD_FONT, size=20)
 
 IMAGE_FULL_WIDTH = CARD_WIDTH * 2
-TEXT_PADDING_Y = 50
-TEXT_PADDING_X = IMAGE_FULL_WIDTH + TEXT_PADDING_Y
+TEXT_PADDING = 50
 
-with open("test-card-image.png", "w") as im_file:
-    full_im = Image.new(mode="RGB", size=(IMAGE_FULL_WIDTH, CARD_HEIGHT), color=(255, 255, 255))
-    front = Image.new(mode="RGB", size=(CARD_WIDTH, CARD_HEIGHT), color=(0, 0, 0))
-    qr_code = qrcode.make(track.get("track").get("external_urls").get("spotify"))
-    qr_im = qr_code.get_image()
-    qr_padding = math.floor((CARD_WIDTH - qr_im.size[0]) / 2)
-    front.paste(qr_im, (qr_padding, qr_padding))
-    back = Image.new(mode="RGB", size=(CARD_WIDTH, CARD_HEIGHT), color=(0, 0, 0))
-    back_draw = ImageDraw.Draw(back)
-    # text_for_back.text(xy=(TEXT_PADDING_X, TEXT_PADDING_Y), text=track.get("track").get("name"), font=font, fill=(255, 255, 255))
-    back_draw.text((TEXT_PADDING_X, TEXT_PADDING_Y), text="ASD", font=font, fill="black", align="center") 
-    full_im.paste(front, (0, 0))
-    full_im.paste(back, (front.size[0], 0))
-    full_im.show()
+full_im = Image.new(mode="RGB", size=(IMAGE_FULL_WIDTH, CARD_HEIGHT), color=(255, 255, 255))
+front = Image.new(mode="RGB", size=(CARD_WIDTH, CARD_HEIGHT), color="black")
+qr_code = qrcode.make(track.get("track").get("external_urls").get("spotify"))
+qr_im = qr_code.get_image()
+qr_padding = math.floor((CARD_WIDTH - qr_im.size[0]) / 2)
+front.paste(qr_im, (qr_padding, qr_padding))
+back = Image.new(mode="RGB", size=(CARD_WIDTH, CARD_HEIGHT), color="white")
+back_draw = ImageDraw.Draw(back)
+back_draw.text((TEXT_PADDING, TEXT_PADDING), text=track.get("track").get("name"), font=font, fill="black", align="left")
+artists = ", ".join(map(lambda artist: artist.get("name"), track.get("track").get("artists")))
+back_draw.text((TEXT_PADDING, TEXT_PADDING * 2), text=artists, font=font, fill="black", align="left")
+released = "Julkaistu % s" % track.get("track").get("album").get("release_date")
+back_draw.text((TEXT_PADDING, TEXT_PADDING * 3), text=released, font=font, fill="black", align="left")
+full_im.paste(front, (0, 0))
+full_im.paste(back, (front.size[0], 0))
+full_im.save("test-card-image.png")
