a24cf758b6baa550d29e389f6621dbcfaf7cae23
Otto Uusihakala <otto.uusihakala@gmail.com>
2025-01-26 21:50:02 +0200

    WIP AWS CDK config for building and deploying hits-timeline-quiz-app

---
+++ hits-timeline-quiz-cdk/test/hits-timeline-quiz-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HitsTimelineQuizCdk from '../lib/hits-timeline-quiz-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/hits-timeline-quiz-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HitsTimelineQuizCdk.HitsTimelineQuizCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
